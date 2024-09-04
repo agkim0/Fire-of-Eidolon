fec44a62e69b7e68fcd96840ea4e59f05048b770
Audrey K <audreygkim0372@gmail.com>
2024-09-04 09:24:37 -0500

    start of project

---
+++ untitled/src/ServerMain.java
@@ -0,0 +1,10 @@
+import java.io.Serializable;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+public class ServerMain implements Serializable {
+    public static void main(String[] args){
+
+    }
+}
