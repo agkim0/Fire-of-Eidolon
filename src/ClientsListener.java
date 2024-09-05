842b6404aad2e8d090d6b6220f06bf29b97b310f
Audrey K <audreygkim0372@gmail.com>
2024-09-05 09:43:25 -0500

    Server Set Up
    - edits in all server and client classes
    - created GameData class

--- src/ClientsListener.java
+++ src/ClientsListener.java
@@ -1,2 +1,26 @@
-public class ClientsListener {
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class ClientsListener implements Runnable,Serializable{
+    private ObjectInputStream is;
+    private ObjectOutputStream os;
+    private FoEFrame frame = null;
+
+    public ClientsListener(ObjectInputStream is, ObjectOutputStream os, FoEFrame frame){
+        this.is = is;
+        this.os = os;
+        this.frame = frame;
+    }
+
+    @Override
+    public void run(){
+        while(true){
+            try{
+                CommandFromServer cfs = (CommandFromServer) is.readObject();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/CommandFromServer.java
+++ src/CommandFromServer.java
@@ -1,2 +1,35 @@
-public class CommandFromServer {
+import java.io.Serializable;
+
+public class CommandFromServer implements Serializable {
+    private int command;
+    private String data="";
+    private GameData gameData;
+    public static final int USERNAME_VALID = 1;
+    public static final int LOBBY_CODE_VALID = 2;
+    public static final int JOINING_HOST_GAME = 3;
+    public static final int CHARACTER_SELECTED = 4;
+    public static final int ACTION = 5;
+    public static final int MESSAGE = 6;
+    public static final int EXCHANGE = 7;
+    public static final int ENDTURN = 8;
+
+
+
+    public CommandFromServer(int command, String data, GameData gameData){
+        this.command = command;
+        this.data = data;
+        this.gameData = gameData;
+    }
+
+    public int getCommand() {
+        return command;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public GameData getGameData() {
+        return gameData;
+    }
 }
