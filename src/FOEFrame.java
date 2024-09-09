09663a389ea878b27b2a31be5c52456bfcc49304
Ana-RFLDG <160946322+Ana-RFLDG@users.noreply.github.com>
2024-09-09 10:24:08 -0500

    imageIOs
    awegwe

--- src/FOEFrame.java
+++ src/FOEFrame.java
@@ -88,7 +88,7 @@ public class FOEFrame extends JFrame implements WindowFocusListener, KeyListener
     private BufferedImage ct_Vestibule = null;
     private BufferedImage ct_VoraciousPlant = null;
     private BufferedImage ct_VoraxsFocus = null;
-    private BufferedImage getCt_VoraxsHeart = null;
+    private BufferedImage ct_VoraxsHeart = null;
     private BufferedImage ct_VoraxsKnowledge = null;
 
     private BufferedImage back_Of_RDC = null;
@@ -148,7 +148,7 @@ public class FOEFrame extends JFrame implements WindowFocusListener, KeyListener
     private JButton btn_Join = new JButton("Join");
     private JButton btn_RB = new JButton("");
 
-    public FOEFrame(/*GameDate gameDate,*/ ObjectInputStream os, String username) throws IOException {
+    public FOEFrame(/*GameDate gameDate,*/ ObjectInputStream os) throws IOException {
         super("FOE Game");
         //this.gameDate = gameDate;
         this.os = os;
@@ -161,13 +161,79 @@ public class FOEFrame extends JFrame implements WindowFocusListener, KeyListener
         setAlwaysOnTop(true);
         setVisible(true);
         logo = ImageIO.read(new File("Game images/Fire of Eidolon Title.png"));
+
         aelfric_Token = ImageIO.read(new File("token pieces/character tokens/aelfric character token.png"));
-        aelfric_Action_Token = ImageIO.read(new File())
+        aelfric_Action_Token = ImageIO.read(new File("character cards/aelfric action.png"));
+        aelfric_Special_Token = ImageIO.read(new File("character cards/aelfric special.png"));
+        aelfric_Character_Card = ImageIO.read(new File("character cards/aelfric character card.png"));
         cecelia_Token = ImageIO.read(new File("token pieces/character tokens/cecelia character token.png"));
+        cecilia_Action_Token = ImageIO.read(new File("character cards/cecilia action.png"));
+        cecilia_Special_Token = ImageIO.read(new File("character cards/cecilia special.png"));
+        cecilia_Character_Card = ImageIO.read(new File("character cards/cecilia character card.png"));
         daga_Token = ImageIO.read(new File("token pieces/character tokens/dage character token.png"));
+        daga_Action_Token = ImageIO.read(new File("character cards/dage action.png"));
+        daga_Special_Token = ImageIO.read(new File("character cards/dage special.png"));
+        daga_Character_Card = ImageIO.read(new File("character cards/dage character card.png"));
         kalistos_Token = ImageIO.read(new File("token pieces/character tokens/kalistos character token.png"));
+        kalistos_Action_Token = ImageIO.read(new File("character cards/kalistos action.png"));
+        kalistos_Special_Token = ImageIO.read(new File("character cards/kalistos special.png"));
+        kalistos_Character_Card = ImageIO.read(new File("character cards/kalistos character card.png"));
         kaylana_Token = ImageIO.read(new File("token pieces/character tokens/kaylana character token.png"));
+        kaylana_Action_Token = ImageIO.read(new File("character cards/kaylana action.png"));
+        kaylana_Special_Token = ImageIO.read(new File("character cards/kaylana special.png"));
+        kaylana_Character_Card = ImageIO.read(new File("character cards/kaylana character card.png"));
         sirius_Token = ImageIO.read(new File("token pieces/character tokens/sirius character token.png"));
+        sirius_Action_Token = ImageIO.read(new File("character cards/sirius action.png"));
+        sirius_Special_Token = ImageIO.read(new File("character cards/sirius special.png"));
+        sirius_Character_Card = ImageIO.read(new File("character cards/sirius character card.png"));
+        back_Of_CC = ImageIO.read(new File("character cards/Hero Card Back Side.png"));
+
+        difficulty_Levels = ImageIO.read(new File("difficulty cards/difficulty levels.png"));
+        df_Beginner = ImageIO.read(new File("difficulty cards/beginner difficulty card.png"));
+        df_Normal = ImageIO.read(new File("difficulty cards/normal difficulty card.png"));
+        df_Extreme = ImageIO.read(new File("difficulty cards/extreme.png"));
+        df_Glitch = ImageIO.read(new File("difficulty cards/glitch"));
+        df_Hard = ImageIO.read(new File("difficulty cards/hard"));
+        df_Heroic = ImageIO.read(new File("difficulty cards/heroic"));
+        df_Legendary = ImageIO.read(new File("difficulty cards/legendary"));
+        df_Nightmare = ImageIO.read(new File("difficulty cards/nightmare"));
+        df_VeryHard = ImageIO.read(new File("difficulty cards/very hard"));
+        scenario_1 = ImageIO.read(new File("difficulty cards/scenario 1.png"));
+        scenario_2 = ImageIO.read(new File("difficulty cards/scenario 2.png"));
+        scenario_3 = ImageIO.read(new File("difficulty cards/scenario 3.png"));
+        scenario_4 = ImageIO.read(new File("difficulty cards/scenario 4.png"));
+
+        ct_AcidJets = ImageIO.read(new File("Game images/Chamber Tiles/Acid jets.png"));
+        ct_ArrowTrap = ImageIO.read(new File("Game images/Chamber Tiles/Arrow Trap.png"));
+        back_Of_CT = ImageIO.read(new File("Game images/Chamber Tiles/Acid jets.png"));
+        ct_DarkSlime = ImageIO.read(new File("Game images/Chamber Tiles/Dark Slime.png"));
+        ct_DenofSnakes = ImageIO.read(new File("Game images/Chamber Tiles/Den of Snakes.png"));
+        ct_Dragonling = ImageIO.read(new File("Game images/Chamber Tiles/Dragonling.png"));
+        ct_FelKnight = ImageIO.read(new File("Game images/Chamber Tiles/Fel Knight.png"));
+        ct_FOE = ImageIO.read(new File("Game images/Chamber Tiles/Fire of Eidolon.png"));
+        ct_FloatingStones = ImageIO.read(new File("Game images/Chamber Tiles/Floating Stones.png"));
+        ct_HallofIlusions = ImageIO.read(new File("Game images/Chamber Tiles/Hall of Ilusions.png"));
+        ct_LaughingShadow = ImageIO.read(new File("Game images/Chamber Tiles/Laughing Shadow.png"));
+        ct_LavaLake = ImageIO.read(new File("Game images/Chamber Tiles/Laval Lake.png"));
+        ct_MimicChest = ImageIO.read(new File("Game images/Chamber Tiles/Mimic Chest.png"));
+        ct_Mindreader = ImageIO.read(new File("Game images/Chamber Tiles/Mindreader.png"));
+        ct_Minotaur = ImageIO.read(new File("Game images/Chamber Tiles/Minotaur.png"));
+        ct_OgerBrute = ImageIO.read(new File("Game images/Chamber Tiles/Oger Brute.png"));
+        ct_ParodoxPuzzle = ImageIO.read(new File("Game images/Chamber Tiles/Paradox puzzle.png"));
+        ct_PengulumBlades = ImageIO.read(new File("Game images/Chamber Tiles/Pengulum Blades.png"));
+        ct_Portal = ImageIO.read(new File("Game images/Chamber Tiles/Portal.png"));
+        ct_Psycomancer = ImageIO.read(new File("Game images/Chamber Tiles/Psychomancer.png"));
+        ct_SPX = ImageIO.read(new File("Game images/Chamber Tiles/Secret Passage X.png.png"));
+        ct_SPY = ImageIO.read(new File("Game images/Chamber Tiles/Secret Passage Y.png"));
+        ct_SkeletonGuards = ImageIO.read(new File("Game images/Chamber Tiles/Skeleton Guards.png"));
+        ct_SphynxsRiddle = ImageIO.read(new File("Game images/Chamber Tiles/Sphynx_s Riddle.png"));
+        ct_SpikedPit = ImageIO.read(new File("Game images/Chamber Tiles/Spiked pit.png"));
+        ct_Vestibule = ImageIO.read(new File("Game images/Chamber Tiles/Start.png"));
+        ct_VoraciousPlant = ImageIO.read(new File("Game images/Chamber Tiles/Voracious Plant.png"));
+        ct_VoraxsFocus = ImageIO.read(new File("Game images/Chamber Tiles/Vorax_s Focus.png"));
+        ct_VoraxsHeart = ImageIO.read(new File("Game images/Chamber Tiles/Vorax_s Heart.png"));
+        ct_VoraxsKnowledge = ImageIO.read(new File("Game images/Chamber Tiles/Vorax_s Knowledge.png"));
+
 
         btn_Host.setBounds(500,500,50,50);
         add(btn_Host);
